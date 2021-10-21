12a2c56fbf87b92422d3011c0dc6b4af56af7c7c
ppotatoo <76911441+ppotatoo@users.noreply.github.com>
2021-10-20 20:49:45 -0400

    it works lets goooo

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+# to get playlist id for a channel:
+# GET https://www.googleapis.com/youtube/v3/channels?id={config['channel_id'}&part=contentDetails&&key={config['api_key']}
+# playlist id is at ["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+
 import requests
 import yaml
 import json
 
-
-
-with open("/home/meizuflux/youtube-webhook/config.yml") as f:
+with open("config.yml") as f:
     config = yaml.safe_load(f)
 
 YT_PARAMS = {
@@ -15,16 +17,16 @@ YT_PARAMS = {
 }
 
 def get_new(new_data) -> list:
-    with open(config["path"] + "/data.json", "r") as f:
+    with open("data.json", "r") as f:
         old_data = json.load(f)
 
-    return new_data["items"].sort(key = lambda x: x not in old_data["items"])
+    return [i for i in new_data["items"][:5] if i not in old_data]
 
 def get_videos():
     req = requests.get("https://www.googleapis.com/youtube/v3/playlistItems", params=YT_PARAMS)
     return req.json()
 
-def post_webhook(url: str, title: str):
+def post_webhook(title, url):
     data = {
         "content": f"@everyone\nNew YouTube video out: {title} -> {url}",
         "username": "YouTube Notifications",
@@ -36,12 +38,11 @@ def post_webhook(url: str, title: str):
     print(f"Posted Webhook: {title} - {url}")
 
 def save_data(data):
-    with open(config["path"] + "/data.json", "w") as f:
-        json.dump(f, data)
+    with open("data.json", "w") as f:
+        json.dump(data["items"], f, indent=4)
 
 def main():
     data = get_videos()
-    print(data)
     new = get_new(data)
     for video in new:
         post_webhook(video["snippet"]["title"], "https://www.youtube.com/watch?v=" + video["id"])
